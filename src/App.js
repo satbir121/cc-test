4ad9f10ac9048b6b630f7e8bf95d7f182aeb6e07
satbir@playment.in <satbir@playment.in>
2019-09-02 18:20:47 +0530

    Complex fun added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,198 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function complex(cond1, cond2, cond3, cond4, cond5, cond6) {
+  if(cond1) {
+    if(cond2) {
+      if(cond3) {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      } else {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      }
+    } else {
+      if(cond3) {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      } else {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      }
+    }
+  } else {
+    if(cond2) {
+      if(cond3) {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      } else {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      }
+    } else {
+      if(cond3) {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      } else {
+        if(cond4) {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        } else {
+          if(cond5) {
+            if(cond6) {
+              console.log("cond6")
+            }
+          } else {
+            if(cond6) {
+              console.log("cond6")
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
